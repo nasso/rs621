a0e3c30ebf7270a0ff9521afe064bcf56ab39006
nasso <nassomails@gmail.com>
2019-07-13 23:00:21 +0200

    Add tests for JSON post parsing and default values

--- src/post.rs
+++ src/post.rs
@@ -88,14 +88,14 @@ pub struct Post {
 
     /// The ID of the post.
     pub id: u64,
-    /// The post's MD5 hash
+    /// The post's MD5 hash. Unavailable for deleted posts.
     pub md5: Option<String>,
     /// The status of the post.
     pub status: PostStatus,
 
     /// Username of the user who uploaded the post.
     pub author: String,
-    /// User ID of the user who uploaded the post.
+    /// User ID of the user who uploaded the post. `None` if unknown.
     pub creator_id: Option<u64>,
     /// When the post was uploaded.
     pub created_at: DateTime<Utc>,
@@ -109,7 +109,7 @@ pub struct Post {
     /// The post's description.
     pub description: String,
 
-    /// If the post has a parent, the ID of the parent post.
+    /// If the post has a parent, the ID of the parent post. `None` if the post has no parent.
     pub parent_id: Option<u64>,
     /// A list of post IDs of this post's children.
     pub children: Vec<u64>,
@@ -126,11 +126,11 @@ pub struct Post {
     /// The post's score.
     pub score: i64,
 
-    /// Absolute URL to the filename.
-    pub file_url: String,
-    /// The post's extension.
+    /// Absolute URL to the filename. Unavailable for deleted posts.
+    pub file_url: Option<String>,
+    /// The post's extension. Unavailable for deleted posts.
     pub file_ext: Option<PostFormat>,
-    /// Size (in bytes) of the post.
+    /// Size (in bytes) of the post. Unavailable for deleted posts.
     pub file_size: Option<u64>,
 
     /// Width of the image.
@@ -138,18 +138,18 @@ pub struct Post {
     /// Height of the image.
     pub height: u64,
 
-    /// Absolute URL of the sample (scaled) filename.
+    /// Absolute URL of the sample (scaled) filename. Unavailable for deleted posts.
     pub sample_url: Option<String>,
-    /// Width of the sample (scaled) image.
+    /// Width of the sample (scaled) image. Unavailable for deleted posts.
     pub sample_width: Option<u64>,
-    /// Height of the sample (scaled) image.
+    /// Height of the sample (scaled) image. Unavailable for deleted posts.
     pub sample_height: Option<u64>,
 
-    /// Absolute URL of the preview (thumbnail) filename.
-    pub preview_url: String,
-    /// Width of the preview (thumbnail) image.
+    /// Absolute URL of the preview (thumbnail) filename. Unavailable for deleted posts.
+    pub preview_url: Option<String>,
+    /// Width of the preview (thumbnail) image. Unavailable for deleted posts.
     pub preview_width: Option<u64>,
-    /// Height of the preview (thumbnail) image.
+    /// Height of the preview (thumbnail) image. Unavailable for deleted posts.
     pub preview_height: Option<u64>,
 }
 
@@ -300,7 +300,7 @@ impl From<&serde_json::Value> for Post {
                 }
             }),
 
-            sources: v["children"].as_array().map_or_else(Vec::new, |v| {
+            sources: v["sources"].as_array().map_or_else(Vec::new, |v| {
                 v.iter().map(|v| v.as_str().unwrap().to_string()).collect()
             }),
 
@@ -310,7 +310,10 @@ impl From<&serde_json::Value> for Post {
             fav_count: v["fav_count"].as_u64().unwrap(),
             score: v["score"].as_i64().unwrap(),
 
-            file_url: v["file_url"].as_str().unwrap().to_string(),
+            file_url: match v["status"].as_str() {
+                Some("deleted") => None,
+                _ => Some(v["file_url"].as_str().unwrap().to_string()),
+            },
             file_ext: v["file_ext"].as_str().map(|v| match v {
                 "jpg" => PostFormat::JPG,
                 "png" => PostFormat::PNG,
@@ -328,7 +331,10 @@ impl From<&serde_json::Value> for Post {
             sample_width: v["sample_width"].as_u64(),
             sample_height: v["sample_height"].as_u64(),
 
-            preview_url: v["preview_url"].as_str().unwrap().to_string(),
+            preview_url: match v["status"].as_str() {
+                Some("deleted") => None,
+                _ => Some(v["preview_url"].as_str().unwrap().to_string()),
+            },
             preview_width: v["preview_width"].as_u64(),
             preview_height: v["preview_height"].as_u64(),
         }
@@ -338,6 +344,223 @@ impl From<&serde_json::Value> for Post {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn post_from_json() {
+        // that's the example JSON output from the official API for the /post/show.json endpoint
+        let example_json = r#"{
+          "id": 8595,
+          "tags": "2005 alley anthro ball bottomwear brown_fur canid canine canis child clothed clothing colored_pencil_(artwork) commentary cub domestic_cat domestic_dog duo english_text felid feline felis female fur ghetto grass happy jessica_willard male mammal mixed_media multicolored_fur outside pants pen_(artwork) playing politics racism shirt sign skirt smile text topwear traditional_media_(artwork) white_fur young",
+          "locked_tags": null,
+          "description": "",
+          "created_at": {
+            "json_class": "Time",
+            "s": 1182709502,
+            "n": 993870000
+          },
+          "creator_id": 46,
+          "author": "Anomynous",
+          "change": 18928032,
+          "source": "Jessica Willard, \"jw-babysteps.jpg\"",
+          "score": 76,
+          "fav_count": 159,
+          "md5": "e9fbd2f2d0703a9775f245d55b9a0f9f",
+          "file_size": 135618,
+          "file_url": "https://static1.e621.net/data/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg",
+          "file_ext": "jpg",
+          "preview_url": "https://static1.e621.net/data/preview/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg",
+          "preview_width": 150,
+          "preview_height": 115,
+          "sample_url": "https://static1.e621.net/data/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg",
+          "sample_width": 800,
+          "sample_height": 616,
+          "rating": "s",
+          "status": "active",
+          "width": 800,
+          "height": 616,
+          "has_comments": true,
+          "has_notes": false,
+          "has_children": true,
+          "children": "128898",
+          "parent_id": null,
+          "artist": [
+            "jessica_willard"
+          ],
+          "sources": [
+            "Jessica Willard, \"jw-babysteps.jpg\"",
+            "http://us-p.vclart.net/vcl/Artists/J-Willard/jw-babysteps.jpg",
+            "http://www.furaffinity.net/view/185399/"
+          ]
+        }"#;
+
+        let parsed = serde_json::from_str::<serde_json::Value>(example_json).unwrap();
+        let post = Post::from(&parsed);
+
+        assert_eq!(post.raw, parsed.to_string());
+
+        assert_eq!(post.id, 8595);
+        assert_eq!(
+            post.md5,
+            Some(String::from("e9fbd2f2d0703a9775f245d55b9a0f9f"))
+        );
+        assert_eq!(post.status, PostStatus::Active);
+
+        assert_eq!(post.author, String::from("Anomynous"));
+        assert_eq!(post.creator_id, Some(46));
+        assert_eq!(post.created_at, Utc.timestamp(1182709502, 993870000));
+
+        assert_eq!(post.artists, vec![String::from("jessica_willard")]);
+        assert_eq!(
+            post.tags,
+            vec![
+                String::from("2005"),
+                String::from("alley"),
+                String::from("anthro"),
+                String::from("ball"),
+                String::from("bottomwear"),
+                String::from("brown_fur"),
+                String::from("canid"),
+                String::from("canine"),
+                String::from("canis"),
+                String::from("child"),
+                String::from("clothed"),
+                String::from("clothing"),
+                String::from("colored_pencil_(artwork)"),
+                String::from("commentary"),
+                String::from("cub"),
+                String::from("domestic_cat"),
+                String::from("domestic_dog"),
+                String::from("duo"),
+                String::from("english_text"),
+                String::from("felid"),
+                String::from("feline"),
+                String::from("felis"),
+                String::from("female"),
+                String::from("fur"),
+                String::from("ghetto"),
+                String::from("grass"),
+                String::from("happy"),
+                String::from("jessica_willard"),
+                String::from("male"),
+                String::from("mammal"),
+                String::from("mixed_media"),
+                String::from("multicolored_fur"),
+                String::from("outside"),
+                String::from("pants"),
+                String::from("pen_(artwork)"),
+                String::from("playing"),
+                String::from("politics"),
+                String::from("racism"),
+                String::from("shirt"),
+                String::from("sign"),
+                String::from("skirt"),
+                String::from("smile"),
+                String::from("text"),
+                String::from("topwear"),
+                String::from("traditional_media_(artwork)"),
+                String::from("white_fur"),
+                String::from("young"),
+            ]
+        );
+        assert_eq!(post.rating, PostRating::Safe);
+        assert_eq!(post.description, String::from(""));
+
+        assert_eq!(post.parent_id, None);
+        assert_eq!(post.children, vec![128898]);
+        assert_eq!(
+            post.sources,
+            vec![
+                String::from("Jessica Willard, \"jw-babysteps.jpg\""),
+                String::from("http://us-p.vclart.net/vcl/Artists/J-Willard/jw-babysteps.jpg"),
+                String::from("http://www.furaffinity.net/view/185399/"),
+            ]
+        );
+
+        assert_eq!(post.has_notes, false);
+        assert_eq!(post.has_comments, true);
+
+        assert_eq!(post.fav_count, 159);
+        assert_eq!(post.score, 76);
+
+        assert_eq!(
+            post.file_url,
+            Some(String::from(
+                "https://static1.e621.net/data/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg"
+            ))
+        );
+        assert_eq!(post.file_ext, Some(PostFormat::JPG));
+        assert_eq!(post.file_size, Some(135618));
+
+        assert_eq!(post.width, 800);
+        assert_eq!(post.height, 616);
+
+        assert_eq!(
+            post.sample_url,
+            Some(String::from(
+                "https://static1.e621.net/data/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg"
+            ))
+        );
+        assert_eq!(post.sample_width, Some(800));
+        assert_eq!(post.sample_height, Some(616));
+
+        assert_eq!(
+            post.preview_url,
+            Some(String::from(
+                "https://static1.e621.net/data/preview/e9/fb/e9fbd2f2d0703a9775f245d55b9a0f9f.jpg"
+            ))
+        );
+
+        assert_eq!(post.preview_width, Some(150));
+        assert_eq!(post.preview_height, Some(115));
+    }
+
+    #[test]
+    fn post_default() {
+        assert_eq!(
+            Post::default(),
+            Post {
+                raw: String::from(""),
+
+                id: 0,
+                md5: None,
+                status: PostStatus::Pending,
+
+                author: String::from(""),
+                creator_id: None,
+                created_at: Utc.timestamp(0, 0),
+
+                artists: Vec::new(),
+                tags: Vec::new(),
+                rating: PostRating::Explicit,
+                description: String::from(""),
+
+                parent_id: None,
+                children: Vec::new(),
+                sources: Vec::new(),
+
+                has_notes: false,
+                has_comments: false,
+
+                fav_count: 0,
+                score: 0,
+
+                file_url: None,
+                file_ext: None,
+                file_size: None,
+
+                width: 0,
+                height: 0,
+
+                sample_url: None,
+                sample_width: None,
+                sample_height: None,
+
+                preview_url: None,
+                preview_width: None,
+                preview_height: None,
+            }
+        );
+    }
 
     #[test]
     fn post_format_display() {
